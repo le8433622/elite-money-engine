ccadc74c549ae3d84dde4214227e4038b68bd94a
le8433622 <le8433622@gmail.com>
2026-04-09 15:28:18 +0700

    Keep README only

--- security/permission_manager.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException, status
-
-def require_admin(user: dict):
-    if user.get("role") != "admin":
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You do not have permission to perform this action. Admin role required."
-        )
-    return True
